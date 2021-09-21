517920913f2e4297938ca40312ed2a519d0c9e14
Juli21v <julianvar212@gmail.com>
2021-09-21 18:57:52 -0500

    primer commit

---
+++ JS/java.js
@@ -0,0 +1,18 @@
+const Cityofcolor = () => {
+    alert("Esta canción es City of color");
+}
+const Freshstart = () => {
+    alert("Esta canción es Fresh start");
+}
+const TidalRush = () => {
+    alert("Esta canción es Tidal rush");
+}
+const NoworNever = () => {
+    alert("Esta canción es Now or Never");
+}
+const MaritimeMemory = () => {
+    alert("Esta canción es Maritime memory");
+}
+const Calamaryinkantation = () => {
+    alert("Esta canción es Calamari inkantation");
+}
